5d82d07c65d4d15ad7e17a9d8d1f8e881f09ba5b
kylemdavies <dav19010@byui.edu>
2023-09-18 10:14:53 -0600

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> nums = new List<int>();
+        int num = 1;
+        while(num != 0){
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            if(num != 0){
+                nums.Add(num);
+            }
+        }
+        int add = 0;
+        foreach (int n in nums){
+            add += n;
+        }
+        Console.WriteLine($"The sum is: {add}");
+        float avg = add / nums.Count;
+        Console.WriteLine($"The average is: {avg}");
+        int lNum = 0;
+        foreach(int n in nums){
+            if (n > lNum){
+                lNum = n;
+            }
+        }
+        Console.WriteLine($"The largest number is: {lNum}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Program p = new Program();
+        p.DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        double sqr = SquareNumber(num);
+        p.DisplayResult(name,sqr);
+    }
+    public void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    public static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    public static int PromptUserNumber()
+    {
+        Console.Write("Please enter your number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    public static double SquareNumber(int number)
+    {
+        double square = Math.Pow(number,2);
+        return square;
+    }
+    void DisplayResult(string name, double sqr)
+    {
+        Console.WriteLine($"{name}, the square of your number is {sqr}");
     }
 }
