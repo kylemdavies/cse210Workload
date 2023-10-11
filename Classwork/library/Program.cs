8f5f654e03c3eeaef067fc12ab28a449cdc351c6
kylemdavies <dav19010@byui.edu>
2023-10-11 10:13:28 -0600

    commit

--- Classwork/library/Program.cs
+++ Classwork/library/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         Bookcase mybookcase = new Bookcase();
-        Book Harry_Potter = new Book("Harry Potter","JK Rowling");
+        Book Harry_Potter = new Book("Harry Potter and the Philosopher's Stone","JK Rowling");
         mybookcase.AddBook(Harry_Potter);
         Harry_Potter.CheckOut();
         Harry_Potter.CheckIn();
