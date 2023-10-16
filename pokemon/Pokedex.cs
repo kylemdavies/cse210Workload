a14606a8fc422b0610fdfe6b05bee39cf4b6c7dd
kylemdavies <dav19010@byui.edu>
2023-10-15 18:26:26 -0600

    commit

---
+++ pokemon/Pokedex.cs
@@ -0,0 +1,9 @@
+class Pokedex
+{
+    private List<Pokemon> _pokemon = new List<Pokemon>();
+    
+    public void AddPokemon(Pokemon p)
+    {
+        _pokemon.Add(p)
+    }
+}
---
+++ pokemon/Pokemon.cs
@@ -0,0 +1,14 @@
+class Pokemon
+{
+    public class Pokemon
+{
+    private int _index;
+    private string _name;
+
+    public Pokemon(int index, string name)
+    {
+        _index = index;
+        _name = name;
+    }
+}
+}
