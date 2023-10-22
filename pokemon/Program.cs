2be39751ab174582deb59a27f4bb1648a908e769
kylemdavies <dav19010@byui.edu>
2023-10-21 19:48:53 -0600

    commit

--- pokemon/Program.cs
+++ pokemon/Program.cs
@@ -1,5 +1,26 @@
 ﻿FileHandler file = new FileHandler();
 Pokedex user = new Pokedex();
 user.SetPokedex(file.ReadFile());
-user.FindPokemonByGen(1);
-user.FindPokemonByType(1,"Fire");
+int Choice = 0;
+while(Choice != 5)
+{
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1: Display Pokedex");
+    Console.WriteLine("2: Find Pokemon");
+    Console.WriteLine("3: Who's that Pokemon?");
+    //Display Pokedex
+    if(Choice == 1)
+    {
+        user.DisplayPokedex();
+    }
+    //Find Pokemon
+    else if(Choice == 2)
+    {
+        int subChoice = 0;
+        //Find Pokemon By name
+        if (subChoice == 1)
+        {
+
+        }
+    }
+}
