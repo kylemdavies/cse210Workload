8c22816d196213468e9eab20ff81347eb5a3b575
kylemdavies <dav19010@byui.edu>
2023-10-30 10:00:58 -0600

    commit

---
+++ pokemon/Team.cs
@@ -0,0 +1,9 @@
+class Team
+{
+    private List<Pokemon> myPokemon = new List<Pokemon>();
+
+    public void AddPokemon(Pokemon pokemon)
+    {
+        myPokemon.Add(pokemon);
+    }
+}
---
+++ pokemon/Trainer.cs
@@ -0,0 +1,10 @@
+class Trainer : Team
+{
+    private string _name;
+    private int _badges;
+    public Trainer(string name, int badges)
+    {
+        _name = name;
+        _badges = badges;
+    }
+}
