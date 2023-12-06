f053f6a198268a436cf6283e47eca66066249b63
kylemdavies <dav19010@byui.edu>
2023-12-06 08:28:28 -0700

    commit

--- final/Foundation2/Product.cs
+++ final/Foundation2/Product.cs
@@ -1,4 +1,14 @@
 public class Product
 {
-
+    string _name;
+    int _ID;
+    double _price;
+    int _quantity;
+    public Product(string name, int ID, double price, int quantity)
+    {
+        _name = name;
+        _ID = ID;
+        _price = price;
+        _quantity = quantity;
+    }
 }
