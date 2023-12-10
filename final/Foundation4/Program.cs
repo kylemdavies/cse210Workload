dc0d078004d4d312a09ad1088be39ff748fcb1f5
kylemdavies <dav19010@byui.edu>
2023-12-10 15:26:46 -0700

    commit

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,6 @@ run.DisplaySummaryKPH();
 Cycling bike = new Cycling("11/03/22", 30, 3.0f);
 bike.DisplaySummaryMPH();
 bike.DisplaySummaryKPH();
-Swimming swim = new Swimming("11/03/22", 30, 3.0f);
+Swimming swim = new Swimming("11/03/22", 30, 10.0f);
 swim.DisplaySummaryMPH();
 swim.DisplaySummaryKPH();
